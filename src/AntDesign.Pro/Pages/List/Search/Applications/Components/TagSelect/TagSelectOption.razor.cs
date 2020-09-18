2988da546e6302adc57d357accf61bd80df91035
Henry.zhang <53967483+1002527441@users.noreply.github.com>
2020-09-18 11:05:19 +0800

    Initialize feat/pages/all

---
+++ src/AntDesign.Pro/Pages/List/Search/Applications/Components/TagSelect/TagSelectOption.razor.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign.Pro.Pages.Lists
+{
+    public partial class TagSelectOption
+    {
+        [Parameter] public string Value { get; set; }
+
+        [Parameter] public bool Checked { get; set; }
+
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        [CascadingParameter] public TagSelect Parent { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Parent.AddOption(this);
+        }
+
+        protected void HandleCheckedChange(bool isChecked)
+        {
+            Checked = isChecked;
+            if (isChecked)
+                Parent.SelectItem(Value);
+            else
+                Parent.UnSelectItem(Value);
+        }
+
+        public void Check(bool isChecked)
+        {
+            Checked = isChecked;
+        }
+    }
+}
---
+++ src/AntDesign.Pro/Pages/User/Login/Login.razor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using AntDesign.Pro.Models;
+using AntDesign.Pro.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign.Pro.Pages
+{
+    public partial class Login
+    {
+        private readonly LoginParamsType _model = new LoginParamsType();
+
+        [Inject] public NavigationManager NavigationManager { get; set; }
+
+        [Inject] public IAccountService AccountService { get; set; }
+
+        [Inject] public MessageService Message { get; set; }
+
+        public void HandleSubmit()
+        {
+            if (_model.UserName == "admin" && _model.Password == "ant.design")
+            {
+                NavigationManager.NavigateTo("/");
+                return;
+            }
+
+            if (_model.UserName == "user" && _model.Password == "ant.design") NavigationManager.NavigateTo("/");
+        }
+
+        public async Task GetCaptcha()
+        {
+            var captcha = await AccountService.GetCaptchaAsync(_model.Mobile);
+            await Message.Success($"获取验证码成功！验证码为：{captcha}");
+        }
+    }
+}
